81861cdd4a136d6956375aa98db19977a3268e3a
rogerloya <31164770+rogerloya@users.noreply.github.com>
2021-10-30 17:30:58 +0200

    Update main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+
+// // Part 1.
+// Step 1
+// document.getElementsByClassName("mess-with-me").style.fontSize = "40px";
+
+// Step 2.
+// document.getElementsByClassName("#mess-with-me").style.backgroundColor = "green";
+
+
+// Step 3.
+const img2 = document.querySelector('#hide-me')
+
+img2.style.display = 'none'
+
+// Step 4.
+
+document.querySelector("#triceratops").style.width = "324px";
+
+// Part 2.
+// Step 1
