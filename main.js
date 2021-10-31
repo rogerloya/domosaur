6642afe05fb7baab4f43d49c0a64588985b7df7b
rogerloya <31164770+rogerloya@users.noreply.github.com>
2021-10-31 20:25:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -18,3 +18,37 @@ document.querySelector("#triceratops").style.width = "324px";
 
 // Part 2.
 // Step 1
+const mwm = document.querySelector('#mess-with-me')
+
+mwm.addEventListener('click', function (){
+    mwm.style.color = 'green';
+})
+
+// Step 2
+
+const dino1 = document.querySelector('#triceratops')
+
+dino1.addEventListener('click', function (){
+    dino1.style.borderColor = 'red';
+})
+
+// Step 3
+const feathDino = document.querySelector('#feathers')
+
+feathDino.addEventListener('click', function (){
+    feathDino.style.MozOpacity=opacity/50;
+})
+
+// Step 4
+const rowC = document.querySelector('#row')
+
+rowC.addEventListener('click', function (){
+    rowC.style.backgroundColor = 'blue';
+})
+
+// Step 5
+const bigDino = document.querySelector("#biggify");
+
+bigDino.addEventListener("mouseover", function (){
+  bigDino.style.width = '200px'
+});
